3aea7aa8c1428be3a5e3cafc1f0ac791f291bb74
Vaishnav V Nair <vaishnav8794@gmail.com>
2023-11-24 11:12:27 +0530

    counting-app

---
+++ counter-app/src/App.js
@@ -0,0 +1,21 @@
+import React, {useState} from 'react';
+import './App.css';
+
+function App() {
+  const [count,setCount]=useState (0);
+     const increment = ()=>{
+      setCount(count+1);
+     };
+     const decrement =()=>{
+      setCount(count-1);
+     };
+  return (
+    <div className="App">
+      <h1>Counting App</h1>
+      <p>Current Count:{count}</p>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+    </div>
+  )
+}
+export default App;
